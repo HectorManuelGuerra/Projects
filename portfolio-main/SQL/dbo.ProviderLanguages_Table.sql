fa96c7b0e63ee015224077debe5af5248fcb357a
Hector Guerra <136126233+HectorManuelGuerra@users.noreply.github.com>
2023-10-10 17:24:00 -0700

    dbo.ProviderLanguages_Table.sql

---
+++ portfolio-main/SQL/dbo.ProviderLanguages_Table.sql
@@ -0,0 +1,15 @@
+USE [wepairhealth]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[ProviderLanguages](
+	[ProviderId] [int] NOT NULL,
+	[LanguageId] [int] NOT NULL
+) ON [PRIMARY]
+GO
+
